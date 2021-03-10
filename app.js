16e10b726ebde3aa1c31420609a0e8eaca931c1a
pawan deore <pawarsatish8292@gmail.com>
2021-03-10 18:25:32 +0530

    console working

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ let btn = document.getElementById('btn');
 
 btn.addEventListener('click',()=>{
     let inp = document.getElementById('inp');
-    let url = 'https://gentle-sierra-66467.herokuapp.com/https://cleanuri.com/api/v1/shorten/https://google.com';
+    let url = 'https://gentle-sierra-66467.herokuapp.com/https://cleanuri.com/api/v1/shorten';
     
 
     fetch(url,{
         method : 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Origin': 'https://gentle-sierra-66467.herokuapp.com/'
         },
-        Origin: 'https://gentle-sierra-66467.herokuapp.com/',
         
-        body : JSON.stringify({url : url})
+        body : JSON.stringify({url : inp.value})
     })
     .then(res => res.json())
     .then(res => {
-        console.log(res)
+        
+            console.log(res)
+        
     })
-    .catch(err => {
+    .catch(err =>{
         console.log(err)
     })
 })
